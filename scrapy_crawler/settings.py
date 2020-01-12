8b180f94409c04ec71dbad9269cab081d9e7f8ef
may083458 <39611343+may083458@users.noreply.github.com>
2020-01-12 19:42:34 +0630

    Update settings.py

--- scrapy_crawler/settings.py
+++ scrapy_crawler/settings.py
@@ -20,19 +20,20 @@ NEWSPIDER_MODULE = 'scrapy_crawler.spiders'
 
 # Obey robots.txt rules
 
+ROBOTSTXT_OBEY = True
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 CONCURRENT_REQUESTS = 2
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY =   1.5 #3
-DOWNLOAD_TIMEOUT = 1
+DOWNLOAD_DELAY =   3 #3
+DOWNLOAD_TIMEOUT = 15
 
 
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 2
-#CONCURRENT_REQUESTS_PER_IP = 2
+CONCURRENT_REQUESTS_PER_DOMAIN = 2
+CONCURRENT_REQUESTS_PER_IP = 2
 
 # Disable cookies (enabled by default)
 #COOKIES_ENABLED = False
@@ -40,6 +41,8 @@ DOWNLOAD_TIMEOUT = 1
 # Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED = False
 
+#RETRY_ENABLED = False
+#RETRY_TIMES = 10 
 # Override the default request headers:
 #DEFAULT_REQUEST_HEADERS = {
 #   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -55,7 +58,7 @@ DOWNLOAD_TIMEOUT = 1
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #DOWNLOADER_MIDDLEWARES = {
-#    'quotes_crawler.middlewares.QuotesCrawlerDownloaderMiddleware': 543,
+#    'scrapy_crawler.middlewares.ScrapyCrawlerDownloaderMiddleware': 543,
 #}
 
 # Enable or disable extensions
@@ -69,17 +72,16 @@ DOWNLOAD_TIMEOUT = 1
 #ITEM_PIPELINES = {
 #    'quotes_crawler.pipelines.QuotesCrawlerPipeline': 300,
 #}
-
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
+AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
+AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
 AUTOTHROTTLE_DEBUG = True
 
