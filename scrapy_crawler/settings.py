d9eebde1f5dc6dee9ef5ae34799231deffbf0e73
may083458 <39611343+may083458@users.noreply.github.com>
2020-01-12 18:51:36 +0630

    12-1-2020

--- scrapy_crawler/settings.py
+++ scrapy_crawler/settings.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Scrapy settings for scrapy_crawler project
+# Scrapy settings for quotes_crawler project
 #
 # For simplicity, this file contains only settings considered important or
 # commonly used. You can find more settings consulting the documentation:
@@ -9,28 +9,29 @@
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
-BOT_NAME = 'scrapy_crawler'
+BOT_NAME = 'quotes_crawler'
 
-SPIDER_MODULES = ['scrapy_crawler.spiders']
-NEWSPIDER_MODULE = 'scrapy_crawler.spiders'
-ALLOWED_DOMAINS = 'ucon.umax.co.jp'
+SPIDER_MODULES = ['quotes_crawler.spiders']
+NEWSPIDER_MODULE = 'quotes_crawler.spiders'
+allowed_domains = 'ucon.umax.co.jp'
 
-
+ITEM_PIPELINES ={'quotes_crawler.pipelines.ExportPipeline' : 100}
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = 'scrapy_crawler (+http://ucon.umax.co.jp)'
+#USER_AGENT = 'quotes_crawler (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = True 
 
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 16
+CONCURRENT_REQUESTS = 2
+
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY =   1 #3
-
-
+DOWNLOAD_DELAY =   3 #3
+DOWNLOAD_TIMEOUT  = 1
+#DOWNLOAD_MAXSIZE =0
 
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 2
@@ -43,10 +44,10 @@ DOWNLOAD_DELAY =   1 #3
 #TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-DEFAULT_REQUEST_HEADERS = {
-   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-   'Accept-Language': 'en',
-}
+#DEFAULT_REQUEST_HEADERS = {
+ #  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+  # 'Accept-Language': 'en',
+#}
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
@@ -57,7 +58,7 @@ DEFAULT_REQUEST_HEADERS = {
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #DOWNLOADER_MIDDLEWARES = {
-   # 'quotes_crawler.middlewares.QuotesCrawlerDownloaderMiddleware': 543,
+#    'quotes_crawler.middlewares.QuotesCrawlerDownloaderMiddleware': 543,
 #}
 
 # Enable or disable extensions
@@ -69,12 +70,12 @@ DEFAULT_REQUEST_HEADERS = {
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 #ITEM_PIPELINES = {
- #   'quotes_crawler.pipelines.QuotesCrawlerPipeline': 100,
+ #  'quotes_crawler.pipelines.QuotesCrawlerPipeline': 100,
 #}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-AUTOTHROTTLE_ENABLED = True
+#AUTOTHROTTLE_ENABLED = True
 # The initial download delay
 AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
