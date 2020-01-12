99a45420aabf58f957f29f12b0e4061cd3b22734
may083458 <39611343+may083458@users.noreply.github.com>
2020-01-12 19:00:13 +0630

    Update settings.py

--- scrapy_crawler/settings.py
+++ scrapy_crawler/settings.py
@@ -20,7 +20,6 @@ NEWSPIDER_MODULE = 'scrapy_crawler.spiders'
 
 # Obey robots.txt rules
 
-ROBOTSTXT_OBEY = True
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 CONCURRENT_REQUESTS = 2
 
